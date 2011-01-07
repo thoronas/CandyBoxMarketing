111a0c6f34643e9ff6f754afb86c2c1e99f1de4f
thoronas <fsquaredesign@gmail.com>
2011-01-07 11:27:13 -0800

    partially fixed comment form, text area works now

--- comments.php
+++ comments.php
@@ -7,8 +7,20 @@
 	$new_defaults = array(
 	
 
-	'fields'               => apply_filters( 'comment_form_default_fields', array('author' => '<div class="blog-form-row clearfix"><div class="blog-form-name"><label>Name</label><span><input type="text" /></span></div>', 'website' => '<div class="blog-form-webs"><label>Website</label><span><input type="text" value="http://" /></span></div></div>') ),
-	'comment_field'        => '<div class="blog-form-row clearfix"><div class="blog-form-comment"><label>Comment</label><span><textarea rows="5" cols="20"></textarea></span></div></div>',
+	'fields'               => apply_filters( 'comment_form_default_fields', 
+	//form field array
+	array(
+	'author' => '<div class="blog-form-row clearfix"><div class="blog-form-name">' . '<label for="author">' . __( 'Name' ) . '
+	' . ( $req ? '<em class="required">*</em>' : '' ) .'
+	
+	</label> <span><input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' /></span></div>', 
+	'website' => '<div class="blog-form-webs"><label>Website</label><span><input type="text" value="http://" /></span></div></div>') ),
+	
+
+	//end of fields array
+
+
+	'comment_field'        => '<div class="blog-form-row clearfix comment-form-comment"><div class="blog-form-comment"><labelfor="comment">' . _x( 'Comment', 'noun' ) . '</label><span><textarea id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea></span></div></div>',
 	'must_log_in'          => '<p class="must-log-in">' .  sprintf( __( 'You must be <a href="%s">logged in</a> to post a comment.' ), wp_login_url( apply_filters( 'the_permalink', get_permalink( $post_id ) ) ) ) . '</p>',
 	'logged_in_as'         => '<p class="logged-in-as">' . sprintf( __( 'Logged in as <a href="%1$s">%2$s</a>. <a href="%3$s" title="Log out of this account">Log out?</a>' ), admin_url( 'profile.php' ), $user_identity, wp_logout_url( apply_filters( 'the_permalink', get_permalink( $post_id ) ) ) ) . '</p>',
 	'comment_notes_before' => '<p class="comment-notes">' . __( '' ) . ( $req ? $required_text : '' ) . '</p>',
